f16002ccff273a0f9fa1add2ff18df247b91de0b
Florian Wolff <florian.wolff@pinuts.de>
2019-03-29 09:25:48 +0100

    no-undef: 0

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ module.exports = {
     extends: [
         '@pinuts/eslint-config-pinuts-base'
     ],
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
+    },
+    root: true,
     rules: {
+        'no-undef': 0,
         'prefer-const': 0,
         'quotes': 0,
         'no-var': 0,
